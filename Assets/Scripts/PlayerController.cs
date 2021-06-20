0141d4183c9be072503e368a475f2c11e34a090e
9li4ch <iosifov.p.v@gmail.com>
2021-06-21 00:45:27 +0300

    RPG 12.156

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,30 @@ using RPG.Movement;
 using RPG.Combat;
 using RPG.Core;
 using System;
+using UnityEngine.EventSystems;
 
 namespace RPG.Control{
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     Health health;
-    private void Awake()
+
+    enum Cursors{
+        None,
+        Movement,
+        Combat,
+        UI
+    }
+    [System.Serializable]
+    struct CursorMaping{
+        public Cursors type;
+        public Texture2D texture;
+        public Vector2 hotspot;
+    }
+
+
+    [SerializeField] CursorMaping[] cursorsMap = null;
+        private void Awake()
     {
         health = GetComponent<Health>();
     }
@@ -21,9 +38,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
         {
-            if(health.IsDead()) return;
+            if(InteractWithUI()){
+                SetCursor(Cursors.UI);
+                return;
+            }
+            if(health.IsDead()){
+                SetCursor(Cursors.None);
+                return;
+            } 
             if(InteractWithCombat()) return;
             if(InteractWithMovement()) return;
+            SetCursor(Cursors.None);
+        }
+
+        private bool InteractWithUI()
+        {
+            return EventSystem.current.IsPointerOverGameObject();
         }
 
         private bool InteractWithCombat()
@@ -36,10 +66,26 @@ public class PlayerController : MonoBehaviour
                     if(Input.GetMouseButton(0)){
                         GetComponent<Fighter>().Attack(target.gameObject);
                     }
+                    SetCursor(Cursors.Combat);
                     return true;
                 }
             }
             return false;
+        }
+
+        private void SetCursor(Cursors cursor)
+        {
+            CursorMaping mapping = GetCursorsMap(cursor);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMaping GetCursorsMap(Cursors type){
+            foreach(CursorMaping Ccursor in cursorsMap){
+                if(Ccursor.type==type){
+                    return Ccursor;
+                }
+            }
+            return cursorsMap[0];
         }
 
         private bool InteractWithMovement()
@@ -51,6 +97,7 @@ public class PlayerController : MonoBehaviour
                 if(Input.GetMouseButton(0)){
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
+                SetCursor(Cursors.Movement);
                 return true;
             }
             return false;
